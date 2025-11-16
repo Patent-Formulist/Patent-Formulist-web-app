729632828d00bc804bcfcf48442a80662cb33c7c
FateyevStanislav <fateev0803@gmail.com>
2025-11-16 21:59:55 +0500

    login, sign, not found pages

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/NotFound.css"; 
+
+function NotFound() {
+  return (
+    <div className="not-found-container">
+      <h1>404 — Страница не найдена</h1>
+      <p>Увы, такой страницы не существует или она была удалена.</p>
+      <div className="not-found-actions">
+        <Link to="/" className="home-link">
+          Вернуться на главную
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
